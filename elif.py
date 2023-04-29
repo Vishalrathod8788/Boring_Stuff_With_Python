4196857742796850ded6118c739f6ed7a99ba5c3
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-04-29 21:03:19 +0530

    Add...

--- elif.py
+++ elif.py
@@ -6,3 +6,7 @@ if name == 'Vishal' :
     print('Hi... Vishal')
 elif age < 12 :
     print('you are not Vishal')
+elif age > 2000 :
+    print('Age is Lasserthan 2000')
+elif age > 100 :
+    print('Age is Lasserthan 100')
