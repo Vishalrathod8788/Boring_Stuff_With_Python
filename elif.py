5deccffe8c5cbac3607e0050077100aaf0a66415
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-04-29 21:05:49 +0530

    Add...

--- elif.py
+++ elif.py
@@ -1,12 +1,21 @@
-name = 'VGR'
+# name = 'VGR'
 
-age = 18
+# age = 18
 
-if name == 'Vishal' :
-    print('Hi... Vishal')
-elif age < 12 :
-    print('you are not Vishal')
-elif age > 2000 :
-    print('Age is Lasserthan 2000')
-elif age > 100 :
-    print('Age is Lasserthan 100')
+# if name == 'Vishal' :
+#     print('Hi... Vishal')
+# elif age < 12 :
+#     print('you are not Vishal')
+# elif age > 2000 :
+#     print('Age is Lasserthan 2000')
+# elif age > 100 :
+#     print('Age is Lasserthan 100')
+
+
+print('Enter a Name ')
+name = input()
+
+if name :
+    print('Thank you for entering a name ')
+else :
+    print('You did not enter a name')
