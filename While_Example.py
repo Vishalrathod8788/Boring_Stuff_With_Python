2e363dab9b1f550a3386e6ba74ad0eef03628f79
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-04-30 22:02:06 +0530

    Add...

--- While_Example.py
+++ While_Example.py
@@ -1,5 +1,13 @@
+# while Statement 
+# spam = 0
+
+# while spam < 5 :
+#     print('Hello World')
+#     spam += 1
+
+# if Statement 
 spam = 0
 
 while spam < 5 :
-    print('Hello World')
+    print('Hello...')
     spam += 1
