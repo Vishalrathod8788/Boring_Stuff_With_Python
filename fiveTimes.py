a8d7dfefcc61b70ade99d22f3db4e470f5e51146
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-04-30 23:35:54 +0530

    Add...

--- fiveTimes.py
+++ fiveTimes.py
@@ -2,11 +2,8 @@
 # for num in range(101) :
 #     total = total + num
 # print(total)
-import random
-print(random.randint(1, 6))
 
-
-# print('My name is')
-# i=0
-# for i in range(5, 17, 2):
-#     print('Jimmy Five Times (' + str(i) + ')')
+print('My name is')
+i=0
+for i in range(5, 17, 2):
+    print('Jimmy Five Times (' + str(i) + ')')
