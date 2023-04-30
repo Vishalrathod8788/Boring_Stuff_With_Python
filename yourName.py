a3ffb8e31f20a701237b09df3fae62752b8e215f
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-04-30 22:12:30 +0530

    Add...

--- yourName.py
+++ yourName.py
@@ -1,8 +1,16 @@
-name = ''
+# name = ''
 
-while name != 'your name':
+# while name != 'your name':
+#     print('Please type your name.')
+    
+#     name = input()
+    
+# print('Thank you!')
+
+while True :
     print('Please type your name.')
-    
     name = input()
-    
+    if name == 'your name':
+        break
+        
 print('Thank you!')
