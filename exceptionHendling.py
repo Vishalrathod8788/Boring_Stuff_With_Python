3810cf835c43b4d2276834d04c51d12324a26816
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-01 23:41:49 +0530

    Add...

--- exceptionHendling.py
+++ exceptionHendling.py
@@ -3,3 +3,10 @@ def div42by(divideBy) :
         return 42 / divideBy
     except ZeroDivisionError :
         print('Error: You tried to divide by zero.')
+
+print(div42by(2))
+print(div42by(12))
+
+print(div42by(0))
+print(div42by(0))
+print(div42by(2))
