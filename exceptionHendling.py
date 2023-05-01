15db0eb24cf14cecfd81d15079b51abe780b18cc
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-01 23:49:05 +0530

    Add...

--- exceptionHendling.py
+++ exceptionHendling.py
@@ -1,13 +1,13 @@
-# def div42by(divideBy) :
-#     try :
-#         return 42 / divideBy
-#     except ZeroDivisionError :
-#         print('Error: You tried to divide by zero.')
+def div42by(divideBy) :
+    try :
+        return 42 / divideBy
+    except ZeroDivisionError :
+        print('Error: You tried to divide by zero.')
 
-# print(div42by(2))
-# print(div42by(12))
-# print(div42by(0))
-# print(div42by(2))
+print(div42by(2))
+print(div42by(12))
+print(div42by(0))
+print(div42by(2))
 
 print('How many cats do you have ?')
 numCat = input()
