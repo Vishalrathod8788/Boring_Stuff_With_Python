eae25c942f0f2935f87cf715260e157cece1b303
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-01 23:26:29 +0530

    Add...

--- magic8ball.py
+++ magic8ball.py
@@ -1,4 +1,23 @@
 import random
 
-def getAnswer(answer ) :
-   
+def getAnswer(answer) :
+    if answer == 1 :
+        return 'it is certain'
+    elif answer == 2 :
+        return 'it is decidedly so'
+    elif answer == 3 :
+        return 'yes'
+    elif answer == 4 :
+        return 'reply hazy try again'
+    elif answer == 5 :
+        return 'your statement is no longer relevant'
+    elif answer == 6 :
+        return 'concentrate and ask again'
+    elif answer == 7 :
+        return 'my reply is no'
+    elif answer == 8 :
+        return 'outlook not so good'
+    elif answer == 9 :
+        return 'very doubtful'
+    else :
+        return 'no idea'
