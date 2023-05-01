c271345a28ee9b9d6f9ac1c69c7732e58662476d
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-01 23:32:25 +0530

    Add...

--- example.py
+++ example.py
@@ -1,3 +1,11 @@
 # print('Hello', end=' ')
 # print('world')
 
+spam = 42 # global variable
+def eggs():
+    spam = 42 # local variable
+    print('some code here')
+    print('some more code')
+    print(spam)
+
+eggs()
