bbce36bf10b4367200bc0611cccb20ba9422a8c1
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-02 16:23:05 +0530

    Add...

--- guess.py
+++ guess.py
@@ -5,5 +5,3 @@ name = input()
 secretNumber = random.randint(1, 20)
 print('Well, ' + name + ', I am thinking of a number between 1 and 20.')
 
-def  random_number(m):
-    return random.randint(0,m)
