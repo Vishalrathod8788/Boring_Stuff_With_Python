c33f354ade06c8c05be99fd06aface3ec9486da3
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-02 16:32:02 +0530

    Add...

--- guess.py
+++ guess.py
@@ -19,4 +19,7 @@ for guessesTaken in range(1, 7):
         
 if guess == secretNumber:
     print('Good job, ' + name + '! You guessed my number in ' + str(guessesTaken) + ' guesses!')
+
+else:
+    print('Nope. The number I was thinking of was ' + str(secretNumber))
     
