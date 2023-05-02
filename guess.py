b555ad36977f9fa173ea708ad88e8ad040eb7a74
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-02 17:05:46 +0530

    Add...

--- guess.py
+++ guess.py
@@ -36,3 +36,14 @@ print('Well, ' + name + ', I am thinking of a number between 1 and 20.')
 
 secretNumber = random.randint(1, 20)
 
+for guessesTaken in range(1, 7) :
+    print('Take a guess.')
+    guess = int(input())
+
+    if guess < secretNumber:
+        print('Your guess is too low.')
+    elif guess > secretNumber:
+        print('Your guess is too high.')
+    else:
+        break
+
