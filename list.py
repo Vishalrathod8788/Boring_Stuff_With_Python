a3b10879d07a5d5fdac3669c2bd06ca5e9b4fce2
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-03 10:58:41 +0530

    Add...

--- list.py
+++ list.py
@@ -31,9 +31,13 @@ spam = ['Cat', 'rat', 'Dog', 'Elephant']
 
 # print(len('Hello'))
 
-print(len([1,2,3,4]))
+# print(len([1,2,3,4]))
 
-print('Hello' + 'World')
+# print('Hello' + 'World')
 
-print([1,2,3,4,5] + [6,7,8,9,10])
+# print([1,2,3,4,5] + [6,7,8,9,10])
  
+
+print([1,2,3,4] * 2)
+
+print('Hello' * 5)
