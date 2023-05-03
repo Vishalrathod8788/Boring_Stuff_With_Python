d681919ddb85544b7997b18b1e3fb33f9d9e34a1
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-03 10:28:14 +0530

    Add...

--- list.py
+++ list.py
@@ -1,7 +1,8 @@
-spam = ['Cat', 'rat', 'Dog', 'Elephant']
+spam = [['Cat', 'rat', 'Dog', 'Elephant'], [1,2,3,4,5,6,7,8,9,10]]
 
-print(spam)
+# print(spam)
 
-print(spam[1])
+# print(spam[1])
 
-print(spam[0])
+# print(spam[0])
+print(spam[1][1])
