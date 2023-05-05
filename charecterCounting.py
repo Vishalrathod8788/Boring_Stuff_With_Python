60bc051fa403e8e2c61023d31940c37f6ceb1a6f
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-05 22:07:19 +0530

    Add...

--- charecterCounting.py
+++ charecterCounting.py
@@ -1,3 +1,5 @@
+import pprint 
+
 
 message = 'it was a bright cold day in May, and the clocks were steiking thirteen'
 
@@ -6,8 +8,5 @@ count = { }
 for character in message.upper():
     count.setdefault(character, 0)
     count[character] = count[character] + 1
-
-
-    
 
 print(count)
