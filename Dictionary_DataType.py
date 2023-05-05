25bd5ce130fe3c2b2e3933c162ac625e7b9e99b5
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-05 21:37:44 +0530

    Add...

--- Dictionary_DataType.py
+++ Dictionary_DataType.py
@@ -9,3 +9,16 @@
 
 
 # Output : Mycat has 162
+
+spam = 42
+demo = 24
+
+print(spam == demo)
+
+spam = {'name' : 'Vishal', 'surname' : 'Rathod', 'age' : str(18)}
+
+demo = {'surname' : 'Rathod', 'name' : 'Vishal', 'age' : str(18)}
+
+print(spam == demo)
+
+print('My name is ' + spam['name'] + ' ' + spam['surname'] + ' and my age is ' + spam['age'])
