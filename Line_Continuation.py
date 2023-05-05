338641fb18944d23bb21ac0a22222396d84bd11f
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-05 20:39:41 +0530

    Add...

--- Line_Continuation.py
+++ Line_Continuation.py
@@ -1,3 +1,11 @@
 spam = ['Apple',
         'Banana',
-        'Orange']
+        'Orange',
+        'Mango']  
+
+print('Four Score and Seven' +  \
+      ' Years ago')
+
+print('Four Score and Seven ' + 'Years ago')
+
+print('Four Score and Seven Years ago')
