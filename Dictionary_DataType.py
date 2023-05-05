3c93c3e1dc0fc6bed6d618d34afa398a909eff31
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-05 21:52:32 +0530

    Add...

--- Dictionary_DataType.py
+++ Dictionary_DataType.py
@@ -13,28 +13,35 @@
 spam = 42
 demo = 24
 
-print(spam == demo)
+# print(spam == demo)
 
 spam = {'name' : 'Vishal', 'surname' : 'Rathod', 'age' : str(18)}
 
 demo = {'surname' : 'Rathod', 'name' : 'Vishal', 'age' : str(18)}
 
-print(spam == demo)
+# print(spam == demo)
 
-print('My name is ' + spam['name'] + ' ' + spam['surname'] + ' and my age is ' + spam['age'])
+# print('My name is ' + spam['name'] + ' ' + spam['surname'] + ' and my age is ' + spam['age'])
 
 # Output : My name is Vishal Rathod and my age is 18
 
-print('name' in spam)
+# print('name' in spam)
 
-print('Height' not in spam)
+# print('Height' not in spam)
 
-print(spam.keys())
-print(spam.values())
-print(spam.items())
+# print(spam.keys())
+# print(spam.values())
+# print(spam.items())
 
-for i in spam.items():
-    print(i)
+# for i in spam.items():
+#     print(i)
 
 
 
+print(demo.get('age', str(0)))
+
+print(demo.get('name', ''))
+
+print(demo.set('name', 'VGRcoder'))
+
+print(demo)
