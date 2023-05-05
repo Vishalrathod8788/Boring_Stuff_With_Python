c8e8d7daa0dc7279b06b03e098d4bbc159f712c0
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-05 21:33:35 +0530

    Add...

--- Dictionary_DataType.py
+++ Dictionary_DataType.py
@@ -2,3 +2,10 @@
 # Created on Wed Apr  3 15:53:10 2019
 
 # Author : Vishal Rathod
+
+myCat = {'size' : 'fat', 'color' : 'red', 'height' : str(162)}
+
+print('Mycat has ' + myCat['height'])
+
+
+# Output : Mycat has 162
