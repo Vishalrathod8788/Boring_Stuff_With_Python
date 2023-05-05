2605cdf714e4958bee4fcddfef1916db324251e9
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-05 22:11:05 +0530

    Add...

--- charecterCounting.py
+++ charecterCounting.py
@@ -1,12 +1,18 @@
 import pprint 
 
 
-message = 'it was a bright cold day in May, and the clocks were steiking thirteen'
+message = 'it was a bright cold day in May, and the clocks were steiking thirteen,ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
+# cout = 0
 count = { }
+for cout in count :
 
-for character in message.upper():
-    count.setdefault(character, 0)
-    count[character] = count[character] + 1
+    for character in message.upper():
+        count.setdefault(character, 0)
+        count[character] = count[character] + 1
 
-print(count)
+# print(count)
+
+pprint.pprint(count)
+
+print
