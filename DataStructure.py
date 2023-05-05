0fdc288135a5aed4d82d14126b5d0227e6e00b40
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-05 22:28:24 +0530

    Add...

--- DataStructure.py
+++ DataStructure.py
@@ -5,4 +5,5 @@ allCat = []
 allCat.append({'name': 'One', 'age': 'str(6)', 'color': 'Black'})
 allCat.append({'name': 'Two', 'age': 'str(4)', 'color': 'Red'})
 allCat.append({'name': 'Three', 'age': 'str(7)', 'color': 'Blue'})
-    
+allCat.append({'name': 'Four', 'age': 'str(10)', 'color': 'Green'})
+allCat.append({'name': 'Five', 'age': 'str(12)', 'color': 'White'})
