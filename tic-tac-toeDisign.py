aae38be4a943ae74db6603e34a22a201bde40550
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-05 23:07:03 +0530

    Add...

--- tic-tac-toeDisign.py
+++ tic-tac-toeDisign.py
@@ -1,7 +1,6 @@
 # Author : Vishal Rathod
 
 import pprint
-
 theBoard  = {'top-L' : ' ', 'top-M' : ' ', 'top-R' : ' ',
              'mid-L' : ' ', 'mid-M' : ' ', 'mid-R' : ' ',
              'low-L' : ' ', 'low-M' : ' ', 'low-R' : ' '}
