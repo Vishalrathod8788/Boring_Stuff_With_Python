c5120a5ef3c8fd4792e48c11c76e06755dd5ad2c
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-07 12:12:59 +0530

    Add...

---
+++ Escape.py
@@ -0,0 +1,14 @@
+print('Hello Friend\'s')
+
+print('Hello there! \nHow are You?\nI\'m fine')
+
+print('Hyy\tHello')
+
+print('How are You\\?')
+
+print("""Dear Friends,
+I am fine.
+Eve's cat has been arrested for catnapping, cat burglary, 
+and extortion.
+Sincerely,
+Hello Friend\'s""")
