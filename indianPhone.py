c5244fe0c0c2a0ac3735fc899229413dadd78ef2
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-07 21:50:38 +0530

    Add...

--- indianPhone.py
+++ indianPhone.py
@@ -1,5 +1,9 @@
 def indianPhone(num) :
     if len(num) != 10:
         return False
-    
+    for i in range(0, 10) :
+        if not num[i].isdecimal() :
+            return False
+    return True
+print(indianPhone('6353008705'))
   
