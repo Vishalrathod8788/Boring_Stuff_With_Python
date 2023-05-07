adac333f9cd56611f15f4bcf1d7cf4930abed530
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-07 14:11:28 +0530

    Add...

--- contains.py
+++ contains.py
@@ -1,2 +1,13 @@
 
 # Check if a value is contained in a list
+
+L = ['One', 'Two', 'Three', 'Four', 'Five']
+
+if 'Two' in L:
+    print('Two is in the list.')
+else:
+    print('Two is not in the list.')
+    
+
+# OK version 🤔 - For loop and a equality check ❌
+
