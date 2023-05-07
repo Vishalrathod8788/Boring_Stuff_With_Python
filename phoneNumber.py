10fa3aeddf67b4235d6a1a59e19dffaa4d8608a7
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-07 21:31:35 +0530

    Add...

--- phoneNumber.py
+++ phoneNumber.py
@@ -2,7 +2,7 @@ def isPhoneNumber(text) :
     if len(text) != 12 :
         return False
     for i in range(0, 3) :
-        if not text.isdigit() :
+        if not text[i].isdigit() :
             return False
 
     if text[3] != '-' :
@@ -11,9 +11,10 @@ def isPhoneNumber(text) :
     for i in range(4, 7) :
         if not text[i].isdecimal() :
             return False
+        
     if text[7] != '-' :
         return False
-        
+
     for i in range(8,12) :
         if not text[i].isdecimal() :
             return False
