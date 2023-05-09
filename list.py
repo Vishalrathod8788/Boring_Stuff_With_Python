8b0da6a949102d90e7717002e22f6f902e3b64d0
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-09 21:21:26 +0530

    Add...

--- list.py
+++ list.py
@@ -1,55 +1,55 @@
-# spam = ['Cat', 'rat', 'Dog', 'Elephant']
+spam = ['Cat', 'rat', 'Dog', 'Elephant']
+
+print(spam)
+
+print(spam[1])
+
+print(spam[0])
+print(spam[0][1]) 
+
+spam[1][1] = 'Hello'
+
+print(spam[-1][-1])
+ 
+
+print(spam[0][1])
+print(spam[0][2])
+
+print(spam[1:3]) 
+
+spam[1 : 3] = ['Monkey', 'Donkey']
 
 # print(spam)
 
-# print(spam[1])
+print(spam[:3])
 
-# print(spam[0])
-# print(spam[0][1]) 
+spam = ['cat', 'bat', 'rat', 'Elephant']
 
-# spam[1][1] = 'Hello'
+del spam[2]
 
-# print(spam[-1][-1])
+print(spam)
+
+print(len('Hello'))
+
+print(len([1,2,3,4]))
+
+print('Hello' + 'World')
+
+print([1,2,3,4,5] + [6,7,8,9,10])
  
 
-# print(spam[0][1])
-# print(spam[0][2])
+print([1,2,3,4] * 2)
 
-# print(spam[1:3]) 
+print('Hello' * 5)
 
-# spam[1 : 3] = ['Monkey', 'Donkey']
+print(str(4))
 
-# # print(spam)
+print(list('Hello'))
 
-# print(spam[:3])
+print('Hello' in ['Hi', 'Hy', 'Hello'])
 
-# spam = ['cat', 'bat', 'rat', 'Elephant']
-
-# del spam[2]
-
-# print(spam)
-
-# print(len('Hello'))
-
-# print(len([1,2,3,4]))
-
-# print('Hello' + 'World')
-
-# print([1,2,3,4,5] + [6,7,8,9,10])
- 
-
-# print([1,2,3,4] * 2)
-
-# print('Hello' * 5)
-
-# print(str(4))
-
-# print(list('Hello'))
-
-# print('Hello' in ['Hi', 'Hy', 'Hello'])
-
-# print(42 not in ['42', 'Hello', 'How Are You'])
-# print(list('Hello'))
+print(42 not in ['42', 'Hello', 'How Are You'])
+print(list('Hello'))
 
 name = 'Zophie a cat'
 
