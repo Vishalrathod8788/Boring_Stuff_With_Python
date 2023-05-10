e8c35d42e5305efa6fe0e31b9ee7574228059403
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-10 18:01:17 +0530

    Add...

--- pattern.py
+++ pattern.py
@@ -7,4 +7,9 @@ print(mo.group())
 
 # mo = batRegex.search('The Adavtures of Bataman')
  
-# print(mo == None) 
+# print(mo == None)  
+
+phoneRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+
+phoneRegex.search('Hello, My name is vishal rathod my phone number is 415-555-1234')
+
