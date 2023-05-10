e5ec4c8b6172742ebb7e75fe0c7aba07172330fa
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-10 19:18:54 +0530

    Add...

--- pattern.py
+++ pattern.py
@@ -44,7 +44,9 @@ import re
 
 # mo = haRegEx.search('He said "HaHaHa"')
 
-phoneRegEx = re.compile(r'((\d\d\d-)?\d\d\d-\d\d\d\d(,)?) {3})')
-mo = phoneRegEx.search('My number is are 415-555-1345 , 254-123-4567 and 456-123-1234')
+# Not Hardword  re.compile(r'((\d\d\d-)?\d\d\d-\d\d\d\d(,)?)((\d\d\d-)?\d\d\d-\d\d\d\d(,)?)((\d\d\d-)?\d\d\d-\d\d\d\d(,)?)
 
-print(mo)
+phoneRegEx = re.compile(r'((\d\d\d-)?\d\d\d-\d\d\d\d(,)?){3}')
+mo = phoneRegEx.search('My number is are 415-555-1345,254-123-4567,456-123-1234')
+
+print(mo.group())
