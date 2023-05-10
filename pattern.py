041705e6e56d7c3ad4c32c7f981bb3b0c56a8e7a
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-10 18:06:56 +0530

    Add...

--- pattern.py
+++ pattern.py
@@ -1,16 +1,20 @@
 import re
 
-batRegex = re.compile(r'Bat(wo)?man')
+# String Search....
 
-mo = batRegex.search('The Adavtures of Batman')
-print(mo.group())
+# batRegex = re.compile(r'Bat(wo)?man')
+
+# mo = batRegex.search('The Adavtures of Batman')
+# print(mo.group())
 
 # mo = batRegex.search('The Adavtures of Bataman')
  
 # print(mo == None)  
 
-phoneRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+# phoneRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
 
-phoneRegex.search('Hello, My name is vishal rathod my phone number is 415-555-1234')
+# phoneRegex.search('Hello, My name is vishal rathod my phone number is 415-555-1234')
 
-demo = phoneRegex.search('Hello, My name is vishal rathod my phone number is 415-555-1234')
+# demo = phoneRegex.search('Hello, My name is vishal rathod my phone number is 415-555-1234')
+
+# print(demo.group()) 
