116a1e1e0fcccf767f646655e47e8488e5025e28
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-10 10:37:44 +0530

    Add...

--- phoneNumber.py
+++ phoneNumber.py
@@ -37,3 +37,6 @@ import re
 #     print('Could not find any phone numbers.')
 
 message = 'Call me 415-555-1011 tomorrow, or at 405-555-9999 or my office Line.'
+
+phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+
