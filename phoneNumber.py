072b2559d767b6c9509a3011398d04b00ca687fc
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-10 10:40:14 +0530

    Add...

--- phoneNumber.py
+++ phoneNumber.py
@@ -40,3 +40,6 @@ message = 'Call me 415-555-1011 tomorrow, or at 405-555-9999 or my office Line.'
 
 phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
 
+mo = phoneNumRegex.search(message)
+
+print(mo.group())
