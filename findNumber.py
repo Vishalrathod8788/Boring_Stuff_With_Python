3811d0e173ebc5ebfc2f54b4b45074b286c9ee0a
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-10 11:18:04 +0530

    Add...

--- findNumber.py
+++ findNumber.py
@@ -3,6 +3,6 @@ import re
 
 phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
 
-mo = phoneNumRegex.findall('Call me 415-555-1011 tomorrow, or at 405-555-9999 or my office Line.')
+mo = phoneNumRegex.findall('Call me 415-555-1011 tomorrow, or at 405-555-9999 or my office Line.') # This is Match Method meance 12 Digit match To Condition is True
 
 print(mo)
