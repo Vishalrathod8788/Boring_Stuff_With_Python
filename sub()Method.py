b4a50545b5c57a2e46af44d2f21a3a29269239ad
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-12 16:50:16 +0530

    Add...

---
+++ sub()Method.py
@@ -0,0 +1,7 @@
+import re
+
+nameRegex = re.compile(r'Agent \w+')
+print(nameRegex.findall('Agent Alice gave the secret documents to Agent Bob.'))
+# ['Agent Alice', 'Agent Bob']
+
+agentNamesRegex = re.compile(r'Agent (\w)\w*')
