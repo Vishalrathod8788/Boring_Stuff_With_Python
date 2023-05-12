0b170f0c498d5d6e35334e1a39e8b0fdd2312376
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-12 12:33:00 +0530

    Add...

--- caretDollarChar.py
+++ caretDollarChar.py
@@ -4,8 +4,12 @@ beginsWithHelloRegex = re.compile(r'^Hello')
 # print(beginsWithHelloRegex.search('Hello There !'))
 # print(beginsWithHelloRegex.search('He said "Hello!"'))
 
-endsWithNumberRegex = re.compile(r'world!$')
-print(endsWithNumberRegex.search('Hello world!'))
+# endsWithNumberRegex = re.compile(r'world!$')
+# print(endsWithNumberRegex.search('Hello world!'))
 
 
 
+
+endsWithNumberRegex = re.compile(r'Hello World!$')
+
+print(endsWithNumberRegex.search('Hello World!'))
