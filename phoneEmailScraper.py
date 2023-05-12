51933aea466844879178755721f07a0d2911fe23
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-12 19:07:27 +0530

    Add...

--- phoneEmailScraper.py
+++ phoneEmailScraper.py
@@ -4,21 +4,24 @@
 
 import re, pyperclip
 
-re.compile(r'''  
+phoneRegex = re.compile(r'''  
 # 415-555-1234, 555-0000, (415) 555-0000, 555-0000 ext 12345, ext. 12345, x12345
-((\d\d\d) | (\(\d\d\d)))?        # area code (optional)
-(\s|-)        # first seprator 
-\d\d\d        # first 3 digits
--        # seprator
-\d\d\d\d        # last 4 digits
-(((ext(\.)?\s) | x)    # extension word-part (optional)
-(\d{2,5}))?       # extension word-number (optional)
+((\d\d\d) | (\(\d\d\d)))?   # area code (optional)
+(\s|-)                      # first seprator 
+\d\d\d                      # first 3 digits
+-                           # seprator
+\d\d\d\d                    # last 4 digits
+(((ext(\.)?\s) | x)         # extension word-part (optional)
+(\d{2,5}))?                 # extension word-number (optional)
 ''', re.VERBOSE )
 
-
-re.compile(r'((\d\d\d) | (\(\d\d\d)))?(\s|-)\d\d\d-\d\d\d\d(((ext(\.)?\s) | x)(\d{2,5}))?')
+# re.compile(r'((\d\d\d) | (\(\d\d\d)))?(\s|-)\d\d\d-\d\d\d\d(((ext(\.)?\s) | x)(\d{2,5}))?')
 
 # TODO : Create a regex for email address
+
+emailRegex = re.compile(r'''
+
+''', re.VERBOSE)
 
 # TODO : Get the text off the clipboard
 
