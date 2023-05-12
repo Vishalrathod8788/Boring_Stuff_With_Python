6453478c8f0f04579ffe9c2d9e247a2123ca745b
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-12 16:54:23 +0530

    Add...

--- sub()Method.py
+++ sub()Method.py
@@ -4,3 +4,5 @@ nameRegex = re.compile(r'Agent \w+')
 print(nameRegex.findall('Agent Alice gave the secret documents to Agent Bob.'))
 # ['Agent Alice', 'Agent Bob']
 
+agentNamesRegex = re.compile(r'Agent (\w)\w*')
+print(agentNamesRegex.sub(r'\1****', 'Agent Alice gave the secret documents to Agent Bob.'))
