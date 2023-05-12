b066bf79a0bc03a47888fa4649ffa5794b9fb883
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-12 12:16:29 +0530

    Add

--- caretDollarChar.py
+++ caretDollarChar.py
@@ -1,5 +1,9 @@
 import re
 
 beginsWithHelloRegex = re.compile(r'^Hello')
-print(beginsWithHelloRegex.search('Hello There !'))
-print(beginsWithHelloRegex.search('He said "Hello!"'))
+# print(beginsWithHelloRegex.search('Hello There !'))
+# print(beginsWithHelloRegex.search('He said "Hello!"'))
+
+endsWithNumberRegex = re.compile(r'world!$')
+print(endsWithNumberRegex.search('Hello world!'))
+
