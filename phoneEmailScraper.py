d3f79417cf8e7c755311af6a871aa707ccd8cfa7
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-12 19:29:40 +0530

    Add...

--- phoneEmailScraper.py
+++ phoneEmailScraper.py
@@ -6,13 +6,15 @@ import re
 
 phoneRegex = re.compile(r'''  
 # 415-555-1234, 555-0000, (415) 555-0000, 555-0000 ext 12345, ext. 12345, x12345
+(
 ((\d\d\d) | (\(\d\d\d)))?   # area code (optional)
 (\s|-)                      # first seprator 
 \d\d\d                      # first 3 digits
 -                           # seprator
 \d\d\d\d                    # last 4 digits
 (((ext(\.)?\s) | x)         # extension word-part (optional)
-(\d{2,5}))?                 # extension word-number (optional)
+(\d{2,5}))? 
+)                # extension word-number (optional)
 ''', re.VERBOSE )
 
 # re.compile(r'((\d\d\d) | (\(\d\d\d)))?(\s|-)\d\d\d-\d\d\d\d(((ext(\.)?\s) | x)(\d{2,5}))?')
@@ -36,6 +38,12 @@ Dr.Barbara Jones x107 870-864-7190 brjones@southark.edu
 # Extract the email/phone numbers from this text
 extractPhone = phoneRegex.findall(text)
 extractEmail = emailRegex.findall(text)
+
+allPhoneNumbers = []
+
+for phoneNumber in extractPhone:
+    allPhoneNumbers.append(phoneNumber[0])
+    
 
 print(extractPhone) 
 print(extractEmail)
