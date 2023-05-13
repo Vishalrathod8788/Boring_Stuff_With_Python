d519a48b8f544f85d660c3a2917b14b8037ae2b7
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-13 21:56:31 +0530

    Add...

--- sub()Method.py
+++ sub()Method.py
@@ -9,4 +9,3 @@ print(nameRegex.findall('Agent Alice gave the secret documents to Agent Bob.'))
 agentNamesRegex = re.compile(r'Agent (\w)\w*')
 print(agentNamesRegex.sub('Vishal 1\****', 'Agent Alice gave the secret documents to Agent Bob.'))
 # Vishal gave the secret documents to Vishal.
-
