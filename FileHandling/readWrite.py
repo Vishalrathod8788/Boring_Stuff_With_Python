c201cf11cbb50dcca7e995a2a3acb094db0a8f72
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-15 17:17:43 +0530

    Add...

--- FileHandling/readWrite.py
+++ FileHandling/readWrite.py
@@ -2,28 +2,28 @@
 
 # ---Read mode
 
-# helloFile = open('/home/vishal/Boring_Stuff_With_Python/Hello.txt')
-# print(helloFile.read())
-# print('Hi \nHello....!')
-# helloFile.close()
+helloFile = open('/home/vishal/Boring_Stuff_With_Python/Hello.txt')
+print(helloFile.read())
+print('Hi \nHello....!')
+helloFile.close()
 
-# helloFile = open('/home/vishal/Boring_Stuff_With_Python/Hello.txt')
-# content = helloFile.read()
-# print(content)
-# helloFile.close()
+helloFile = open('/home/vishal/Boring_Stuff_With_Python/Hello.txt')
+content = helloFile.read()
+print(content)
+helloFile.close()
 
 # ---Write mode 
 
-# helloFile = open('/home/vishal/Boring_Stuff_With_Python/Hyy.txt', 'w')
-# print(helloFile.write('Hello Vishal\n'))
-# print(helloFile.write('Hello Vishal\n'))
-# print(helloFile.write('Hello Vishal\n'))
-# print(helloFile.write('Hello Vishal\n'))
-# helloFile.close()
+helloFile = open('/home/vishal/Boring_Stuff_With_Python/Hyy.txt', 'w')
+print(helloFile.write('Hello Vishal\n'))
+print(helloFile.write('Hello Vishal\n'))
+print(helloFile.write('Hello Vishal\n'))
+print(helloFile.write('Hello Vishal\n'))
+helloFile.close()
 
 import shelve
 
-# ---Shelve
+# ---Shelve       Dictionary-like Shelf value
 
 shelfFile = shelve.open('mydata')
 
