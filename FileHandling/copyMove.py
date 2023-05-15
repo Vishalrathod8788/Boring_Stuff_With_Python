9a4eb6e848c532948e88a7fd3aabf317b665db8f
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-15 17:51:59 +0530

    Add...

--- FileHandling/copyMove.py
+++ FileHandling/copyMove.py
@@ -2,12 +2,14 @@ import shutil
 
 # Copy File And Directory
 
-# print(shutil.copy('/home/vishal/Boring_Stuff_With_Python/FileHandling/readWrite.py', '/home/vishal/Boring_Stuff_With_Python/spamspamspam.txt'))
+print(shutil.copy('/home/vishal/Boring_Stuff_With_Python/FileHandling/readWrite.py', '/home/vishal/Boring_Stuff_With_Python/spamspamspam.txt'))
 
-# print(shutil.copytree('/home/vishal/Boring_Stuff_With_Python/FileHandling', '/home/vishal/Boring_Stuff_With_Python/spamspamspam'))
+print(shutil.copytree('/home/vishal/Boring_Stuff_With_Python/FileHandling', '/home/vishal/Boring_Stuff_With_Python/spamspamspam'))
 
 # Move File And Directory
 
-# print(shutil.move('/home/vishal/Boring_Stuff_With_Python/readWrite.py', '/home/vishal/Boring_Stuff_With_Python/FileHandling/'))
+print(shutil.move('/home/vishal/Boring_Stuff_With_Python/readWrite.py', '/home/vishal/Boring_Stuff_With_Python/FileHandling/'))
+
+# Rename File And Directory
 
 print(shutil.move('/home/vishal/Boring_Stuff_With_Python/FileHandling/readingWriting.py' ,'/home/vishal/Boring_Stuff_With_Python/FileHandling/readWrite.py'))
