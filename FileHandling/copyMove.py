7c28397c8cf60a6a4a88f756b544eb530d9a4262
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-15 17:45:28 +0530

    Add...

--- FileHandling/copyMove.py
+++ FileHandling/copyMove.py
@@ -2,9 +2,10 @@ import shutil
 
 # Copy File And Directory
 
-print(shutil.copy('/home/vishal/Boring_Stuff_With_Python/FileHandling/readWrite.py', '/home/vishal/Boring_Stuff_With_Python/spamspamspam.txt'))
+# print(shutil.copy('/home/vishal/Boring_Stuff_With_Python/FileHandling/readWrite.py', '/home/vishal/Boring_Stuff_With_Python/spamspamspam.txt'))
 
-print(shutil.copytree('/home/vishal/Boring_Stuff_With_Python/FileHandling', '/home/vishal/Boring_Stuff_With_Python/spamspamspam'))
+# print(shutil.copytree('/home/vishal/Boring_Stuff_With_Python/FileHandling', '/home/vishal/Boring_Stuff_With_Python/spamspamspam'))
 
 # Move File And Directory
 
+print(shutil.move('/home/vishal/Boring_Stuff_With_Python/readWrite.py', '/home/vishal/Boring_Stuff_With_Python/FileHandling/'))
