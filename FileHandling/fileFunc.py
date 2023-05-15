977e83baccb55fe48f209187a670f712f9c1bbc4
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-15 10:20:55 +0530

    Add...

--- FileHandling/fileFunc.py
+++ FileHandling/fileFunc.py
@@ -2,6 +2,12 @@ import os
 
 print(os.path.join('Folder1', 'Folder2', 'File1.png'))
 
-print(os.sep())
+print(os.sep)
 
 print(os.getcwd())
+
+# Absolute and Relative Paths
+
+print(os.path.abspath('File1.png'))
+
+print(os.path.relpath('File1.png'))
