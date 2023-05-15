00f3286cd4967d721735896536c9f509ec148fe6
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-15 16:54:29 +0530

    Add...

--- FileHandling/readWrite.py
+++ FileHandling/readWrite.py
@@ -4,8 +4,11 @@
 
 helloFile = open('/home/vishal/Boring_Stuff_With_Python/Hello.txt')
 print(helloFile.read())
+print('Hi \nHello....!')
 helloFile.close()
 
 helloFile = open('/home/vishal/Boring_Stuff_With_Python/Hello.txt')
 content = helloFile.read()
 print(content)
+helloFile.close()
+
