209ae770861b8230f53616def350de8346a1e84d
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-15 16:58:11 +0530

    Add...

--- FileHandling/readWrite.py
+++ FileHandling/readWrite.py
@@ -2,16 +2,21 @@
 
 # Read mode
 
-helloFile = open('/home/vishal/Boring_Stuff_With_Python/Hello.txt')
-print(helloFile.read())
-print('Hi \nHello....!')
-helloFile.close()
+# helloFile = open('/home/vishal/Boring_Stuff_With_Python/Hello.txt')
+# print(helloFile.read())
+# print('Hi \nHello....!')
+# helloFile.close()
 
-helloFile = open('/home/vishal/Boring_Stuff_With_Python/Hello.txt')
-content = helloFile.read()
-print(content)
-helloFile.close()
+# helloFile = open('/home/vishal/Boring_Stuff_With_Python/Hello.txt')
+# content = helloFile.read()
+# print(content)
+# helloFile.close()
 
 # Write mode 
 
-helloFile = open('/home/vishal/Boring_Stuff_With_Python/Hello.txt', 'w')
+helloFile = open('/home/vishal/Boring_Stuff_With_Python/Hyy.txt', 'w')
+print(helloFile.write('Hello Vishal'))
+print(helloFile.write('Hello Vishal'))
+print(helloFile.write('Hello Vishal'))
+print(helloFile.write('Hello Vishal'))
+helloFile.close()
