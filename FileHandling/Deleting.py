12faac9a065bb3aecd79c54a216672143b1cd6e8
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-16 15:50:23 +0530

    Add File...

--- FileHandling/Deleting.py
+++ FileHandling/Deleting.py
@@ -4,12 +4,13 @@ print(os.getcwd())
 
 # print(os.rmdir('/home/vishal/Boring_Stuff_With_Python/spamspamspam/'))
 
-shutil.rmtree('/home/vishal/Boring_Stuff_With_Python/spamspamspam/')
+# shutil.rmtree('/home/vishal/Boring_Stuff_With_Python/spamspamspam/')
 
 # Dry Run...
 
-# for filename in os.listdir() :
-#     if filename.endswith('.py'):                      # This code will not Execute 
-#         print(filename)
+for filename in os.listdir() :
+    if filename.endswith('.txt'):
+        print(filename)
+        os.unlink(filename)
 
 
