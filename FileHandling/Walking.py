a8749ab231e538a7c3af477622bede5a71e64e69
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-17 21:53:30 +0530

    Add File...

--- FileHandling/Walking.py
+++ FileHandling/Walking.py
@@ -1,9 +1,8 @@
 import os
 
-for folderName, subfolders, filenames  in os.walk('/home/vishal/Boring_Stuff_With_Python/File') :
-
-    print('The Folder is ' + folderName)
-    print('The Demo is ' + subfolders)
+for folderName, subfolders, filenames  in os.walk('/home/vishal/Boring_Stuff_With_Python/') :
+    # print('The Folder is ' + folderName)
+    # print('The Demo is ' + subfolders)
     for filename in filenames :
         print('The current file is ' + filename)
         print('The Subfolder in ' + folderName + 'are: ' + str(subfolders))
