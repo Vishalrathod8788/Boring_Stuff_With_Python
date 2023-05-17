57e16bb68f8173f00125e204b9c45d7cb230c6f5
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-17 22:14:45 +0530

    Add File...

--- Debuging/BoxPrint.py
+++ Debuging/BoxPrint.py
@@ -12,6 +12,9 @@ def boxPrint(symbol, width, height) :
 
     if len(symbol) != 1:
         raise Exception("Error: Symbol must be a single character.")
+    
+    if (width < 2) or (height < 2) :
+        raise Exception("Error: Width and height must be greater than 2.")
 
     print(symbol * width)
     for i in range(height - 2) :
@@ -19,6 +22,6 @@ def boxPrint(symbol, width, height) :
     
     print(symbol * width)
 
-boxPrint('*', 15, 5) 
-boxPrint('O', 15, 5)
+boxPrint('*', 1, 1) 
+# boxPrint('O', 15, 5)
 # boxPrint('**', 15, 5)
