f937122f61c58903322c7df37790b3daa70f262b
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-17 22:09:40 +0530

    Add File...

---
+++ Debuging/BoxPrint.py
@@ -0,0 +1,19 @@
+"""
+
+***************
+*             *
+*             *
+*             *
+***************
+
+"""
+
+def boxPrint(symbol, width, height) :
+    print(symbol * width)
+
+    for i in range(height - 2) :
+        print(symbol + (' ' * (width - 2)) + symbol)
+    
+    print(symbol * width)
+
+boxPrint('*', 15, 5)
