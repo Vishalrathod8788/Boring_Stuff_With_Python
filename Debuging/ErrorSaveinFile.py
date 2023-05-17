8158862c0ce01d34c1079ca5126e3e50f1ac4f8f
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-17 22:40:01 +0530

    Add File...

---
+++ Debuging/ErrorSaveinFile.py
@@ -0,0 +1,10 @@
+
+import traceback
+
+try :
+    raise Exception('This is the error message.')
+except :
+    errorFile = open('error_log.txt', 'a')
+    errorFile.write(traceback.format_exc())
+    errorFile.close()
+    print('The Traceback info was written error_log.txt')
