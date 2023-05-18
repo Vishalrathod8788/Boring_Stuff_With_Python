ab9b533d39d985f6bf6609ee9ae98be97df0b631
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-18 10:49:19 +0530

    Add File...

---
+++ Debuging/Logging.py
@@ -0,0 +1,31 @@
+# There are Five Levels Of 'Log'
+# debug(lowest)
+# info
+# worning
+# error
+# critical(highest)
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.disable(logging.CRITICAL)
+
+
+logging.debug('Start Of Program')
+
+def factorial(n):
+    logging.debug('Start of factorial(%s)' % (n))
+    total = 1
+    for i in range(1, n) :
+
+        total *= i
+        n+1
+        logging.debug('i is (%s)' % (i))
+        logging.debug('total is (%s)' %(total))
+
+    logging.debug('Reurn Value (%s)' % (total))
+    return total
+
+print(factorial(5))
+
+logging.debug('End of program')
