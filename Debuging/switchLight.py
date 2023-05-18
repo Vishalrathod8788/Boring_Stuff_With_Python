57f62e3b7f13723f66f5139dcb171f9fb453e9a9
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-05-18 10:10:24 +0530

    Add File...

--- Debuging/switchLight.py
+++ Debuging/switchLight.py
@@ -1,1 +1,14 @@
 market_2nd = {'ns' : 'Green', 'ew' : 'red'}
+
+def switchLight(intersection) :
+
+    for key in intersection.keys() :
+        if intersection[key] == 'Green' :
+            intersection[key] = 'red'
+        elif intersection[key] == 'red' :
+            intersection[key] = 'Green'
+
+
+print(market_2nd)
+switchLight(market_2nd)
+print(market_2nd)
