4de199b2b58c6f490beefeb5850159e66b5a0a3b
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-06-21 22:30:08 +0530

    Add...

--- elif.py
+++ elif.py
@@ -2,15 +2,14 @@
 
 # age = 18
 
-if name == 'Vishal' :
-    print('Hi... Vishal')
-elif age < 12 :
-    print('you are not Vishal')
-elif age > 2000 :
-    print('Unlike you, Vishal is not an undead, immortal vampire ')
-elif age > 100 :
-    print('You are not Vishal, Grannie')
-
+# if name == 'Vishal' :
+#     print('Hi... Vishal')
+# elif age < 12 :
+#     print('you are not Vishal')
+# elif age > 2000 :
+#     print('Unlike you, Vishal is not an undead, immortal vampire ')
+# elif age > 100 :
+#     print('You are not Vishal, Grannie')
 
 print('Enter a Name ')
 name = input()
