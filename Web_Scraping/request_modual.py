6c081dd89f72092ef4a872bf3fe3340f5883e345
Vishal Rathod <vishalrathod1562004@gmail.com>
2023-08-05 21:22:23 +0530

    Add PlayFile

--- Web_Scraping/request_modual.py
+++ Web_Scraping/request_modual.py
@@ -11,3 +11,12 @@ print(res.text[:500])
 # res.raise_for_status()
 
 print(res.raise_for_status())
+
+
+playFile = open('RomeoAndJuliet.txt', 'wb')
+
+for chunk in res.iter_content(100000):
+    playFile.write(chunk)
+
+
+
